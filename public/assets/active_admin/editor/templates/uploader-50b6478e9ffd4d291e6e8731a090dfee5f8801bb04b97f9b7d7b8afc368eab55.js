13e75d4d0903a5d9589837688476b89a4a6dc52b
Cristian Georgescu <georgescu.cristi@gmail.com>
2016-10-13 22:33:53 +0300

    precompiled assets deleted from local

--- public/assets/active_admin/editor/templates/uploader-50b6478e9ffd4d291e6e8731a090dfee5f8801bb04b97f9b7d7b8afc368eab55.js
+++
@@ -1,2 +0,0 @@
-(function() { this.JST || (this.JST = {}); this.JST["active_admin/editor/templates/uploader"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<div class="upload">\n  or\n  <input type="file" />\n  <img src="',  spinner ,'" class="spinner"></img>\n</div>\n<div style="clear: both;"></div>\n');}return __p.join('');};
-}).call(this);
--- public/assets/tinymce/preinit-be8a4e5712a1e1e1bea82f7c5b39a9adf1e638ba6601176728f1a3be34fd122b.js
+++
@@ -1,5 +0,0 @@
-window.tinyMCEPreInit = window.tinyMCEPreInit || {
-  base:   '/assets/tinymce',
-  query:  '3.5.8.3',
-  suffix: ''
-};
