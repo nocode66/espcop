369f5e918827c7bcd308e24f7f225adcbadcd20e
Cristian Georgescu <georgescu.cristi@gmail.com>
2016-11-10 21:25:22 +0200

    add pdf

--- app/models/tinymce_asset.rb
+++ app/models/tinymce_asset.rb
@@ -1,4 +1,4 @@
 class TinymceAsset < ApplicationRecord
     has_attached_file :image
-    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    validates_attachment_content_type :image, :content_type => ["application/pdf","image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
--- public/assets/tinymce/plugins/uploadimage/langs/en_dlg.js
+++ public/assets/tinymce/plugins/uploadimage/langs/en_dlg.js
@@ -1,12 +1,12 @@
 tinyMCE.addI18n('en.uploadimage_dlg', {
-  title: 'Insert image',
-  header: "Insert image",
-  input:  "Choose an image",
+  title: 'Insert file',
+  header: "Insert file",
+  input:  "Choose a file",
   uploading: "Uploading…",
   blank_input: "Must choose a file",
   bad_response: "Got a bad response from the server",
   blank_response: "Didn't get a response from the server",
   insert: "Insert",
   cancel: "Cancel",
-  alt_text: "Image description"
+  alt_text: "File description"
 });
