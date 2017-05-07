e274882e7bc472c5838f0375fe0a21bd9530c625
Cristian Georgescu <georgescu.cristi@gmail.com>
2017-05-07 18:07:11 +0300

    fixed card update

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,14 +2,15 @@ class CardsController < ApplicationController
   before_action :authenticate_user!
   def update
     customer=Stripe::Customer.retrieve(current_user.stripe_id)
-    customer.source= params[:stripe_token]
-    current_user.update(
-      card_last4: params[:card_last4],
-      card_exp_month: params[:card_exp_month],
-      card_exp_year: params[:card_exp_year],
-      card_type: params[:card_brand]
-      )
-    current_user.save
-    redirect_to edit_user_registration_path, notice:"Successfully updated your card"
+    card=customer.sources.retrieve(customer['sources']['data'].first['id']).delete
+    customer.sources.create(source: params['stripe_card_token'])
+    customer=Stripe::Customer.retrieve(current_user.stripe_id)
+    stripe_customer_params = JSON.parse customer.to_s
+    current_user.card_last4= stripe_customer_params['sources']['data'].first['last4']
+    current_user.card_type=stripe_customer_params['sources']['data'].first['brand']
+    current_user.card_exp_month=stripe_customer_params['sources']['data'].first['exp_month']
+    current_user.card_exp_year=stripe_customer_params['sources']['data'].first['exp_year']
+    current_user.save!
+    redirect_to edit_user_registration_path, notice:"Successfully updated your card!"
   end
 end
