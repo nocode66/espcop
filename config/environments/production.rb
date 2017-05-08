5e4d46f019c8ed6747c32ce79a9e0e00bded3ebc
Cristian Georgescu <georgescu.cristi@gmail.com>
2017-05-08 15:09:13 +0300

    asset stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+   
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -22,6 +22,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.serve_static_files = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.precompile += %w( charges.js map.js contact-form
 Rails.application.config.assets.precompile += %w( rich/editor.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "vendor")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
