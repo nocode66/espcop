a48d9fe8a2a1994a325cbb69eb04853e00cac25f
Cristian Georgescu <georgescu.cristi@gmail.com>
2017-05-08 16:54:57 +0300

    assets fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 Rails.application.config.assets.precompile += %w( charges.js map.js contact-form.js)
 Rails.application.config.assets.precompile += %w( rich/editor.css )
 # Add additional assets to the asset load path
